4fd3e23e5d09e9f70cf8ea5527d57fd1c8971530
jorgeoj <jorge2002oj@gmail.com>
2023-10-28 10:30:41 +0200

    Hecho el fxml del login (No código)

--- src/main/java/com/example/gestionpracticasempresa/HelloController.java
+++ src/main/java/com/example/gestionpracticasempresa/HelloController.java
@@ -1,11 +1,22 @@
 package com.example.gestionpracticasempresa;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private PasswordField pswdField;
+    @FXML
+    private Button btnIniciarSesion;
+    @FXML
+    private Label info;
 
     @FXML
     protected void onHelloButtonClick() {
